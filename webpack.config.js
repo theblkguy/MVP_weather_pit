97817772bc1d7a13e3d5bd6ae9d2de8db63d4ec4
theblkguy <allblk13@gmail.com>
2025-06-10 11:17:24 -0500

    moving to a new repository

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 const path = require('path');
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/weather_pit');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
 
 module.exports = {
   mode: 'development',
